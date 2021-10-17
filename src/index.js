04f068ad22aa648708dad6549974c5aa0223a260
pnguyen224 <pnguyen224@dxc.com>
2021-10-17 20:38:41 +0700

    Bootstrap Commit

--- src/index.js
+++ src/index.js
@@ -5,15 +5,24 @@ const handlebars  = require('express-handlebars');
 const app = express()
 const port = 30000
 
+app.use(express.static(path.join(__dirname,'public')));
+
 //HTTP logger
 app.use(morgan('combined'))
 //Template engine
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars(
+  {
+    extname: '.hbs'
+  }
+));
+app.set('view engine', 'hbs');
 app.set('views',path.join(__dirname,'resources/views'))
 
 app.get('/', (req, res) => {   
   res.render('home');
+})
+app.get('/news', (req, res) => {   
+  res.render('news');
 })
 
 app.listen(port, () => {
